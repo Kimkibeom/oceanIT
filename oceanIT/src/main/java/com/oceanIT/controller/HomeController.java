9834cf9a89bdab2b6480b45edd56775a5f7d475a
kimkibeom <kimki1124@metabuild.co.kr>
2016-01-24 02:58:20 +0900

    2016.01.24 Mybatis Sample complete

--- oceanIT/src/main/java/com/oceanIT/controller/HomeController.java
+++ oceanIT/src/main/java/com/oceanIT/controller/HomeController.java
@@ -1,15 +1,11 @@
 package com.oceanIT.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	Logger log = Logger.getLogger(this.getClass());
+    
+    @RequestMapping(value="/sample/openSampleList.do")
+    public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("");
+        log.debug("인터셉터 테스트");
+         
+        return mv;
+    }
 	
 }
